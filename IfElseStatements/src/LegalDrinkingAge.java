aca41df57e508f43ef17496edaf6883699d663b5
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-04-06 08:47:00 -0400

    Im gonna get cancelled on twitter for this

--- IfElseStatements/src/LegalDrinkingAge.java
+++ IfElseStatements/src/LegalDrinkingAge.java
@@ -6,11 +6,17 @@ public class LegalDrinkingAge {
     Scanner scanner = new Scanner(System.in);
     System.out.printf("Welcome to the Kool Kidz Bar, you're only allowed in if you are %d years old, or older.\nPlease enter your age: ",legalAge);
     int age = Integer.parseInt(scanner.nextLine());
-    if (age >= legalAge) {
-      System.out.println("Welcome on in!");
-    } else {
+    if (!(age >= legalAge)) {
       int diff = legalAge - age;
       System.out.printf("Woah woah woah! You're not old enough to come in.\nPlease wait %d years before coming back!",diff);
+      return;
     }
+    System.out.print("Please enter your gender (male/female): ");
+    String gender = scanner.nextLine();
+    if (gender.toLowerCase().equals("male")) {
+      System.out.println("Sorry, but you can't come in. It's ladies night.");
+      return;
+    }
+    System.out.println("Welcome in!");
   }
 }
