a0285833e6a3084d27b5b2d1357e7b26708839b2
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2022-04-13 09:01:43 -0400

    Add assignments project and start first assignment

---
+++ IfElseStatements/src/Agree.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Agree {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Play again? (y/n): ");
+    String choice = scanner.nextLine().toLowerCase();
+    if (choice.equals("y")) {
+      System.out.println("Excellent!");
+    } else {
+      System.out.println("See ya nerd lol");
+    }
+  }
+}
---
+++ SchoolAssignments/src/TaxationCalculator.java
@@ -0,0 +1,28 @@
+/* Simple taxation calculation program.
+ * Calculates tax on a salary, and has optional donation to charity.
+ * 
+ * Steps for using: Input name, "Last, First"
+ * Input hourly pay, "13.40"
+ * Input hours worked, "80"
+ * Input agreeance to pay for charity, "Y", "N"
+ * 
+ * Program will then calculate tax, and return:
+ * Last, First
+ * Hours worked: 80    Hourly Rate: 13.40
+ * Gross Pay: 1340
+ * Tax Bracket: D      Tax Deducted: $388.60
+ * Net Pay: $951.40
+ * 
+ * Charity was not selected so $20 was not deducted
+*/
+
+import java.util.Scanner;
+public class TaxationCalculator {
+  public static void main(String[] args) {
+    // Setup a scanner object to collect user input, via the standard System.in stream.
+    Scanner scanner = new Scanner(System.in);
+    
+    // Close the scanner object when finished to clean up resources.
+    scanner.close();
+  }
+}
